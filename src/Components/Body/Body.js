f93d1dd9d9048875ac8de399074993c954a0780f
Naslu kk <naslukk@gmail.com>
2023-12-30 11:17:14 +0530

    Google ads tag added secon

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -122,14 +122,18 @@ function Body() {
                     </div>
                 ) : ""}
             </div>
-            <amp-ad width="100vw" height="320"
-                type="adsense"
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6498728354273010"
+                crossorigin="anonymous"></script>
+            <!-- top -->
+            <ins class="adsbygoogle"
+                style="display:block"
                 data-ad-client="ca-pub-6498728354273010"
                 data-ad-slot="7302819061"
-                data-auto-format="rspv"
-                data-full-width="">
-                <div overflow=""></div>
-            </amp-ad>
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({ });
+            </script>
         </div>
     )
 }
